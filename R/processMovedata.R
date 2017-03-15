55bd0b837e72623a0ba242a9e38d7ac27da9afd7
PMahoney29 <pmahoney29@gmail.com>
2017-03-15 14:13:46 -0700

    Updated processMovedata function to allow for the estimation of trajectory statistics and exporting an SPDF.

--- R/processMovedata.R
+++ R/processMovedata.R
@@ -4,28 +4,38 @@
 ##' 
 ##' @param movedata movement data - can be a \code{Move} object (or stack) from movebank
 ##' @param idcolumn name of the id column - depends on properties of the movebank data.  The default "individual_id" is often good.  Other options are "deployment_id" or maybe ""individual_id" ... this is confusing!
-##' @param proj4 the projection for conversion to UTM.  If it is left empty and \code{movedata} is a \code{Move} object, it will carry over the projection from the original data.  If \code{movedata} is a data frame, it will use the "WGS84" projection, using the midpoint of the longitudes and latitudes in the data, i.e. (min(long,lat) + max(long,lat))/2.
-##' @param dailymean whether or not to compute the daily mean - useful for the migration analysis
+##' @param proj4 the projection (as a crs string) for input data.  If it is left empty and \code{movedata} is a \code{Move} object, it will carry over the projection from the original data.  If \code{movedata} is a data frame, it will use the "WGS84" projection, using the midpoint of the longitudes and latitudes in the data, i.e. (min(long,lat) + max(long,lat))/2.
+##' @param projTo a crs string for (re)projecting input data.
 ##' @param keepCols vector of column names to retain in output (e.g., c('deployment_id', 'sex')).
+##' @param getVT a logical indicating whether or not to estimate trajectory statistics (default = TRUE).
+##' @param geoDist a logical (default is FALSE) indicating whether or not to estimate distance using geographic coordinates along an ellipsoid (see pointDistance in raster package).
+##' @param units if getVT = TRUE, a string representing the time units used to define time periods (default = 'hour').
+##' @param returnSPDF if the returned object should be in a SPDF format.
+##' @param dailymean whether or not to compute the daily mean - useful for the migration analysis.
 ##' 
-##' @return Returns a data frame with columns "id", "x", "y", (UTM easting/northing), "lat", "lon", "time", "day" (day since Jan 1 of first year), "day.time". 
+##' @return Returns a data frame with columns "id", "time" (POSIXct), '"lon", "lat", "x", "y", "time", and any columns named in keepCols argument.  If getVT = TRUE, "z", "z.start", "z.end" (complex numbers indicating location), 'stepLength' (step length), 'phi' and 'theta' (absolute and relative turning angles, respectively), 't.start', 't.end', 't.mid' (numeric time), dT (difference in time over step), 'v' (velocity, default meters / hour), 't.mid.POSIX' (time mid point in POSIX), and if dailymean = TRUE, "day" (day since Jan 1 of first year) and "day.time". 
 ##' 
 ##' @example ./examples/example1.r
-##' @seealso \link{map.track}, \link{plot.movetrack}
+##' @seealso \link{map.track}, \link{plot.movetrack}, \link{SpatialPointsDataFrame}, \link{pointDistance}
 
 
-processMovedata <- function(movedata, 
+processMovedata <- function(movedata,
+                            xyNames = c('location_long', 'location_lat'),
                             idcolumn = "individual_id",
                             proj4 = NULL, projTo = NULL,
-                            dailymean = FALSE,
-                            keepCols = NULL){
+                            keepCols = NULL,
+                            getVT = FALSE, geoDist = FALSE, units = 'hour',
+                            returnSPDF = TRUE, 
+                            dailymean = FALSE){
   
   if(inherits(movedata, "Move") | inherits(movedata, "MoveStack")){
     if(is.null(proj4)) proj4 <- as.character(movedata@proj4string)
     movedata <- as(movedata, "data.frame")
   } else if(is.null(proj4)) {
-    lon.center <- round(mean(range(movedata$location_long)))
-    lat.center <- round(mean(range(movedata$location_lat)))
+    #lon.center <- round(mean(range(movedata$location_long)))
+    #lat.center <- round(mean(range(movedata$location_lat)))
+    lon.center <- round(mean(range(movedata[, xyNames[1]])))
+    lat.center <- round(mean(range(movedata[, xyNames[2]])))
     proj4 <- paste0("+proj=lcc +lat_1=",lat.center," +lat_2=",lat.center," +lon_0=",lon.center," +ellps=WGS84")
   }
   
@@ -38,12 +48,13 @@ processMovedata <- function(movedata,
   }  else names(movedata)[names(movedata) == idcolumn] <- "id"
 
   if(!is.null(projTo)){
-    xy <- project(cbind(movedata$location_long, movedata$location_lat), projTo)
+    #xy <- project(cbind(movedata$location_long, movedata$location_lat), projTo)
+    xy <- project(as.matrix(movedata[, xyNames]), projTo)
     movedata$x <- xy[,1]
     movedata$y <- xy[,2]
   } else {
-      movedata$x <- movedata$location_long
-      movedata$y <- movedata$location_lat
+      movedata$x <- movedata[, xyNames[1]]
+      movedata$y <- movedata[, xyNames[2]]
   }
   
   movedata.setup <- (mutate(movedata, id = factor(id), time = ymd_hms(timestamp)) %>% 
@@ -51,23 +62,72 @@ processMovedata <- function(movedata,
       day1 <- ymd(paste(year(df$time[1]), 1, 1))
       mutate(df, day = as.numeric(floor(difftime(df$time, day1))),
              day.date = ymd_hms(paste(year(df$time[1]),1,1,12,0,0)) + ddays(day),
-             lon = location_long, 
-             lat = location_lat)
+             lon = df[, xyNames[1]], 
+             lat = df[, xyNames[2]])
     }))
   
   if(dailymean){
-    movedata.processed <- ddply(movedata.setup, c("id", "day", "day.date"), summarize, 
+    md.processed <- ddply(movedata.setup, c("id", "day", "day.date"), summarize, 
                                   time = mean(time),
                                   lon = mean(lon), 
                                   lat = mean(lat), 
                                   x = mean(x), y = mean(y))
     if (!is.null(keepCols))
-      movedata.processed <- merge(movedata.processed, 
+      md.processed <- merge(md.processed, 
                                   unique(movedata.setup[, c('id', 'day.date', keepCols)]), 
                                   by = c('id', 'day.date'), all.x = T)
-    } else movedata.processed <- movedata.setup[,c("id", "time", "lon", "lat", "x", "y", keepCols)]
+    } else md.processed <- movedata.setup[,c("id", "time", "lon", "lat", "x", "y", keepCols)]
   
-  class(movedata.processed) <- c("movetrack", "data.frame")
+  if (getVT) {
+    if (grepl('+proj=longlat', proj4) & is.null(projTo) & !geoDist)
+      warning('Estimating distance using LongLat without accounting for ellipsoid, stepL and v are likely biased. Consider changing geoDist argument to TRUE.')
+
+    if (is.null(units))
+      stop('If estimating trajectories, please specify a time unit.')
+    
+    ids <- unique(md.processed$id)
+    mo <- c()
+    for (i in 1:length(unique(ids))) {
+      mi <- subset(md.processed, id = ids[i])
+      mi$z <- mi$x + (0+1i) * mi$x
+      z.start <- mi$z[-nrow(mi)]
+      z.end <- mi$z[-1]
+      if (!geoDist)
+        stepL <- Mod(diff(mi$z)) else
+          stepL <- pointDistance(as.matrix(mi[-nrow(mi), c('lon', 'lat')]),
+                                 as.matrix(mi[-1, c('lon', 'lat')]), longlat = TRUE)
+      phi <- Arg(diff(mi$z))
+      theta <- c(NA, diff(phi))
+      
+      t.mid.POSIX <- mi$time[-nrow(mi)] + diff(mi$time)/2
+      
+      if(inherits(mi$time, "POSIXt")) {  
+        time <- as.numeric(mi$time-mi$time[1])
+        time <- time/ifelse(units == "sec", 1, 
+                                      ifelse(units == "min", 60, 
+                                             ifelse(units == "hour", 60*60, 
+                                                    ifelse(units == "day", 60*60*24, 
+                                                           stop("Invalid time unit.")))))
+      }
+      
+      t.start <- time[-nrow(mi)]
+      t.end <- time[-1]
+      dT <- t.end-t.start
+      v <- stepL/as.vector(dT)
+      t.mid <- (t.start + t.end)/2
+      trajTab <- data.frame(z.start, z.end, stepL, phi, theta, 
+                       t.start, t.end, t.mid, dT, v, t.mid.POSIX)
+      trajTab <- rbind(NA, trajTab)
+      mo <- rbind(mo, cbind(mi, trajTab))
+    }
+    md.processed <- mo
+  }
   
-  return(movedata.processed)
+  if (returnSPDF) {
+    p4s <- CRS(ifelse(is.null(projTo), proj4, projTo))
+    md.processed <- SpatialPointsDataFrame(md.processed[, c('x', 'y')], data = md.processed, proj4string = p4s)
+    #class(md.processed) <- c("movetrack", "SpatialPointsDataFrame")
+  } else class(md.processed) <- c("movetrack", "data.frame")
+  
+  return(md.processed)
 }
--- examples/example1.r
+++ examples/example1.r
@@ -3,16 +3,26 @@
 ## Not run: 
 # login <- movebankLogin(username="xxxx", password="xxxx")
 
-ge1 <- getMovebankData(study="ABoVE: HawkWatch International Golden Eagles", login=login) #one bird, one deployment
-ge1.dailymean <- processMovedata(ge1,  id = "individual_id")
+# Pull data from movebank
+ge1 <- getMovebankData(study="ABoVE: HawkWatch International Golden Eagles", login=login)     #multiple birds
 
-ge2 <- getMovebankData(study="Aquila chrysaetos interior west N. America, Craigs, Fuller", animalName="629-26704", login=login) 
+# Estimate daily mean locations
+ge1.dailymean <- processMovedata(ge1,  id = "individual_id", dailymean = TRUE, getVT = TRUE, geoDist = FALSE)
+
+# Include trajectory statistics
+ge1.traj <- processMovedata(ge1,  id = "individual_id", getVT = TRUE)
+
 # one bird, three deployments
-ge2.dailymean <- processMovedata(ge2,  id = "deployment_id")
+ge2 <- getMovebankData(study="Aquila chrysaetos interior west N. America, Craigs, Fuller", animalName="629-26704", login=login) 
 
-# a few simple methods:
+# Estimate daily mean locations
+ge2.dailymean <- processMovedata(ge2,  id = "individual_id", dailymean = TRUE, geoDist = FALSE)
+
+# Plot daily movements (currently only displays 1 individual)
 plot(ge2.dailymean)
 
-## Not run:
-# load("./hidden/ge.rda")
-ge.dailymean <- processMovedata(ge)
+# Return a SpatialPointsDataFrame
+ge2.spdf <- processMovedata(ge2,  id = "individual_id", dailymean = TRUE, returnSPDF = T)
+
+# Include trajectory statistics
+ge2.traj <- processMovedata(ge2,  id = "individual_id", getVT = TRUE)
