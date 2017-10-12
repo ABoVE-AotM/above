5269d4d60606f4a7d50da2ba07ae4b50c00c8be8
EliGurarie <eliezg@u.washington.edu>
2017-10-12 17:14:18 -0400

    Created new function for generating grid for using Env-Data

---
+++ R/createEnvDataGrid.R
@@ -0,0 +1,46 @@
+##' Create gridded data for querying Movebank Env-Data 
+##' 
+##' Takes a vector of latitudes and longitudes (typically evenly spaced) and a vector of times and generates the strictly formatted data frame needed to upload to Env-Data to obtain covariates for movebank.  Essentially, an R-based substitute for the "gridded data" tool. 
+##' 
+##' @param {lats,lons} vectors of latitude and longitude for the desired grid.  Can also be a single location. 
+##' @param {start,finish} vectors of POSIX times of start and finish for the query. 
+##' @param dt time interval between desired time points. 
+##' @param savefile whether to save a csv file
+##' @param fileout name of csv file to save
+##' 
+##' @return Either nothing (if csv file saved) or the character string data frame with correct formatting. 
+##' @examples 
+##'  require(lubridate)
+##'  lats <- seq(38.8, 39.0, length = 40)
+##'  lons <- seq(-77.12, -76.91, length = 40) 
+##'  start <- ymd("2014-01-01")
+##'  finish <- ymd("2014-12-31")
+##'  dt <- ddays(20)
+##'  example <- createEnvDataGrid(lats, lons, start, finish, dt, savefile = FALSE)
+##'  head(example)
+##' 
+##' @export
+
+
+createEnvDataGrid <- function(lats, lons, start, finish, dt, 
+                              savefile = FALSE, fileout = "latlontime.csv"){  
+  
+  times <- seq(start + hms("00:00:00.001"), finish + hms("00:00:00.001"), length = (finish - start)/dt) 
+  times.formatted <- paste0(as.character(times), ".000") 
+  
+  latlontime <- expand.grid(lats, lons, times.formatted) 
+  latlongtime.formatted <- data.frame(timestamp = latlontime$Var3, 
+                             'location-long' = latlontime$Var2,
+                             'location-lat' = latlontime$Var1,
+                             'height-above-ellipsoid' = "")
+  if(savefile){ 
+    cat("Saving formatted data to", fileout)
+    write.csv(latlongtime.formatted, file = fileout)
+  } else
+  return(latlongtime.formatted)
+}  
+  
+  
+#' example
+ 
+  
