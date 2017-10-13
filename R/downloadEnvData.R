9798084bdcb8b9ce5310b3753a75e8f3cc563694
Elie <eliezg@uw.edu>
2017-10-13 17:29:10 -0400

    Added downloading function

---
+++ R/downloadEnvData.R
@@ -0,0 +1,18 @@
+##' Download and return annotated data from EnvData
+##' 
+##' @param header the header file outputted by \code{\link{uploadEnvDataRequest.R}}
+##' @param returndata whether to return the data directly
+##' @param filename filename (and location) to save annotated data to.  If left empty, user will be prompted.
+##' 
+##' @return the annotated data frame
+##' @export
+
+
+downloadEnvData <- function(header, returndata = TRUE, filename = NULL){
+  basicurl <- header['Location']
+  downloadurl <- gsub("status.jsp", "download.jsp", basicurl)
+  if(is.null(filename))
+    filename <- readline(prompt="Please provide filename for annotated data: ")
+  download.file(downloadurl, filename)	
+  return(read.csv(filename))
+}
