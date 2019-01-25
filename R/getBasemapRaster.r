2b1bd991a26a95efe35df3e4cfb3f1f086462560
EliGurarie <eliezg@u.washington.edu>
2019-01-25 18:14:23 -0500

    MOre updates to getBasemapRaster

--- R/getBasemapRaster.r
+++ R/getBasemapRaster.r
@@ -2,13 +2,13 @@
 #' 
 #' This function obtains an RGB raster from any of the available base maps in the (incredible!) open source leaflet package.  It is meant to replace - in a way - the \code{\link{ggmap}} function, which now requires an API key with Google.  It is in some ways more flexible: you can set the lat and long limits strictly, it returns a projected raster which can be reprojected to any other resolution, and it can access any of the remarkable diversity of high quality mapping option at:  http://leaflet-extras.github.io/leaflet-providers/preview/.  Note that it creates a "png" and "html" and places it in a given directory, and does not delete them. 
 #'  
-#' @param {xmin, ymin, xmax, ymax} Limits (in longitude and latitude) of desired map raster.
+#' @param {xmin,xmax,ymin,ymax} Limits (in longitude and latitude) of desired map raster.
 #' @param map.types Character specification for the base maps. see http://leaflet-extras.github.io/leaflet-providers/preview/ for available options. Favorites include: \code{Esri.WorldPhysical} (default), \code{Esri.WorldTerrain}, \code{Esri.NatGeoWorldMap}
 #' @param filename name of png and html files
-#' @param directory directory to save the htlm and png files
+#' @param directory directory to save the html and png files
 #' @param {width,height} approximate width and height of final raster; this is VERY approximate because the final raster is cropped around the desired limits and because there is some 
+#' @param zoom this is the zoom argument from \code\link{webshot}; it also has some complicated effect on the resolution of the final image.
 #' @param plotme whether or not to plot the raster with \code{\link{plotRGB}}. Note that high resolution rasters are reduced in rendering within R by default ... this can be modified with \code{\link{plotRGB}} options.  
-#' @param ... Additional paramters to pass to \code{\link{mapview}}
 #' @return An RGB raster, i.e. one with three levels for each of the colors
 #' @export
 #' @examples
@@ -16,15 +16,14 @@
 #' SEalaska.topo <- getBasemapRaster(-138,-130, 56, 60, "OpenTopoMap")
 #' # for a ggPlot use this function (from RStoolbox): 
 #' ggRGB(SEalaska.topo, 1, 2, 3, coord_equal = FALSE)
-#' # DC map, high resolution
-#' dc.natgeo <- getBasemapRaster(-77.5,-76.5, 38.5, 39.25, "Esri.NatGeoWorldMap", zoom = 8, plotme = FALSE)
-#' ggRGB(dc.natgeo, 1, 2, 3, coord_equal = FALSE)
+#' # labeled DC map, high resolution
+#' dc.natgeo <- getBasemapRaster(-77.5,-76.5, 38.5, 39.25, map.types = "Esri.NatGeoWorldMap", width = 1000, height = 1000, zoom = 8)
 
 
 getBasemapRaster <- function(xmin, xmax, ymin, ymax, map.types = "Esri.WorldPhysical",
                               directory = ".", 
                               filename = "basemap",
-                              width = 1000, height = 1000,
+                              width = 1000, height = 1000, zoom = 1, 
                               plotme = TRUE, ...)
 {
   # creating a white rectangular donut
@@ -43,7 +42,8 @@ getBasemapRaster <- function(xmin, xmax, ymin, ymax, map.types = "Esri.WorldPhys
   # create local html  
   mapshot(basemap, url = mapurl)
   # create png from local html
-  webshot(url = mapurl, file = mappng, vwidth = width*2, vheight = height*2, zoom = sqrt(width/1000))
+  webshot(url = mapurl, file = mappng, vwidth = width*2, vheight = height*2, zoom = zoom)
+          #zoom = sqrt(width/1000))
   m <- brick(paste0(directory, '/', filename, '.png'))
   
   # trim off legend and other mapview text at bottom 
